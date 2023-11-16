422263c19f6996a6919e579eb162248dc387bfda
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-11-16 21:18:19 +0700

    update redux thunk

--- src/redux/auth/authThunk.jsx
+++ src/redux/auth/authThunk.jsx
@@ -31,3 +31,43 @@ export const registerInitial = createAsyncThunk(
     }
   }
 );
+// Todo1 : Handle login account into firebase
+export const loginInitial = createAsyncThunk(
+  "auth/login",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      // Login with email,password
+      const { user } = await auth.signInWithEmailAndPassword(email, password);
+
+      showSuccessToast("Login Success");
+      // return all data user redux toolkit
+      return user;
+    } catch (error) {
+      // Info error
+      showErrorToast(handleAuthError(error));
+      // If error return error redux toolkit
+      console.error("Error during registration:", error);
+      return rejectWithValue(error);
+    }
+  }
+);
+// Todo 3: Handle login google account into firebase
+export const loginGoogleInitial = createAsyncThunk(
+  "auth/login/google",
+  async (_, { rejectWithValue }) => {
+    try {
+      // Login with email,password
+      const { user } = await auth.signInWithPopup(googleAuthProvider);
+
+      showSuccessToast("Login google Success");
+      // return all data user redux toolkit
+      return user;
+    } catch (error) {
+      // Info error
+      showErrorToast(handleAuthError(error));
+      // If error return error redux toolkit
+      console.error("Error during registration:", error);
+      return rejectWithValue(error);
+    }
+  }
+);
